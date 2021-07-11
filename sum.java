60a94724281486f9e2f419f23d1eb0ee0d234b05
Indranil Roy <indranilroy836@gmail.com>
2021-07-11 14:55:59 +0530

    my name

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ public class sum{
     {
         int a=5;
         int b=6;
-        System.out.println("the sum is:"+(a+b) );
+        System.out.println("the sum is:"+(a+b)+"my name is" );
     }
 }
